86d1231dfe26a0cefd595f89ad2891c36da23f72
Pierre-Louis Serré <pierre-louisserre@Hostacc906029e5f.proxynet.ladapt.local>
2024-10-17 20:35:36 +0200

    replace visual studio 2022 mac by vscode

---
+++ GeneratorPKMTeam/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+using GeneratorPKMTeam;
+
+string data = File.ReadAllText(@"PKMType.json");
+var datas = JsonSerializer.Deserialize<PKMDatas>(data);
+Console.Read();
