d3f9bd8dcc918812da89295020ea3db4a8a358b1
Pierre-Louis Serré <pierre-louisserre@Hostacc906029e5f.proxynet.ladapt.local>
2024-10-23 20:37:43 +0200

    relier tout le projet pour que ca marche à partir de la CLI

--- GeneratorPKMTeam/Infrastructure/Services/PKMTypePersistence.cs
+++ GeneratorPKMTeam/Infrastructure/Services/PKMTypePersistence.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GeneratorPKMTeam.Domain.Port.Driven;
+using GeneratorPKMTeam.Infrastructure.Connector;
 
 namespace GeneratorPKMTeam.Infrastructure.Services
 {
-    public class PKMTypePersistence
+    public class PKMTypePersistence : IPKMTypePersistence
     {
 
+        public PKMDatas GetPKMDatas()
+        {
+            var pkms = new PKMTypeJson();
+            return pkms.GetPKMDatas();
+        }
     }
 }
--- GeneratorPKMTeam/Program.cs
+++ GeneratorPKMTeam/Program.cs
@@ -1,10 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Text.Json;
-using GeneratorPKMTeam;
+using GeneratorPKMTeam.Domain.Handler;
 using GeneratorPKMTeam.Infrastructure.Connector;
+using GeneratorPKMTeam.Infrastructure.Services;
 
-// string data = File.ReadAllText(@"PKMType.json");
-// var datas = JsonSerializer.Deserialize<PKMDatas>(data);
 var connector = new PKMTypeJson();
-var result = connector.GetPKMDatas();
+var PMKPersistence = new PKMTypePersistence();
+
+var loadPKMTypes = new LoadPKMTypes(PMKPersistence);
+var selectPKMTypes = new SelectPKMTypes();
+var fightPKMTypes = new FightPKMTypes();
+var resultFightPKMTypes = new ResultFightPKMTypes();
+var gererResultatTiragePKMTypes = new GererResultatTiragePKMTypes();
+var handler = new GeneratePKMTeamHandler(loadPKMTypes, selectPKMTypes, fightPKMTypes, resultFightPKMTypes,
+                gererResultatTiragePKMTypes);
+
+handler.Generate();
+
+var tiragesAAfficher = handler.TiragePKMTypes;
+
+for (int i = 0; i < tiragesAAfficher.Count; i++)
+{
+    var tirage = tiragesAAfficher[i];
+    int tirageNumero = i + 1;
+    Console.WriteLine("Tirage n°" + tirageNumero);
+    foreach (var pkmType in tirage.PKMTypes)
+    {
+        Console.WriteLine(pkmType.Nom);
+    }
+    Console.WriteLine("-------FIN Tirage n°" + tirageNumero);
+}
+
 Console.Read();
