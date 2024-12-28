e7fcad694b1e917d594ad5b440e83720088a1838
Pierre-Louis Serré <pierre-louisserre@MacBook-Air-de-Pierre-Louis.local>
2024-12-28 17:44:51 +0100

    commencer à faire la partie calcul

---
+++ GeneratorPKMTeam/Domain/Handler/SelectionPKM/DeterminerMeilleurPKMParStats.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GeneratorPKMTeam.Domain.Models;
+
+namespace GeneratorPKMTeam.Domain.Handler.SelectionPKM
+{
+    public class DeterminerMeilleurPKMParStats : IDeterminerMeilleurPKMParStats
+    {
+        public PKM Calculer(List<PKM> pkmsAComparer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
