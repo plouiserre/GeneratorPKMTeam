1e6eb7302c28745f52f7c5e02a7c8d59208150b1
Pierre-Louis Serré <pierre-louisserre@MacBook-Air-de-Pierre-Louis.local>
2025-01-10 16:53:02 +0100

    changer la composition du fichier RelPKMTypePersonas en le simplfiant et le renommant

---
+++ GeneratorPKMTeamTest/Utils/Personas/EnumsTests.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GeneratorPKMTeam;
+
+namespace GeneratorPKMTeamTest.Utils.Personas
+{
+
+    public enum FrequenceRelPKMType
+    {
+        Peu,
+        Quelques,
+        Beaucoup
+    }
+
+    public enum NomListRelPKMType
+    {
+        Faibles,
+        Dangereux
+    }
+}
